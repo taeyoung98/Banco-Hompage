509b68d0a3f8233ab3f3bcec020a268f6b41aaed
minju <mjko8903@gmail.com>
2021-02-16 21:47:22 +0900

    hologram return

--- src/components/Main/Hologram.js
+++ src/components/Main/Hologram.js
@@ -1,4 +1,21 @@
 const Hologram = () => {
-  return <div className="main__hologram">홀로그램, vr, ar</div>;
+  return (
+    <div className="main__hologram">
+      <div className="hologram__text-box">
+        <div className="hologram__text-box-inside">
+          <img
+            className="up-on-scroll__x-left"
+            src="https://www.banco.id/res/img/banco_logo.png"
+            alt="main__logo"
+          />
+          <h3 className="up-on-scroll__x-left hologram__title"></h3>
+          <p className="up-on-scroll__x-left"></p>
+        </div>
+      </div>
+      <div className="hologram__image-box">
+        <img className="up-on-scroll__y-right" src={} alt="" />
+      </div>
+    </div>
+  );
 };
 export default Hologram;
